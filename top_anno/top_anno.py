93afdaa19fe94396390c97fc3ce606780025b1f4
funfunchen <funfun.chen@gmail.com>
2019-02-28 22:31:19 -0500

    add logger

--- top_anno/top_anno.py
+++ top_anno/top_anno.py
@@ -4,7 +4,15 @@ import numpy as np
 import pandas as pd
 import argparse
 import time
+import misc
+import os
+import dask
 
+
+tmp_dir = os.path.join(os.getcwd(), tmp)
+dask.config.set({'temporary_directory': 'tmp_dir'})
+
+## arguments parse
 parser = argparse.ArgumentParser(description='Top_anno: DASK based annotator for the Topmed project')
 parser.add_argument('--ref_db', type=str, help='path for the refs')
 parser.add_argument('--res', type=str, help='path for the results to be annotated')
@@ -14,6 +22,11 @@ parser.add_argument('--prefix', default='top_anno', type=str, help='Prefix for o
 parser.add_argument('-o', '--output_dir', default='.', help='Output directory')
 args = parser.parse_args()
 
+logger = SimpleLogger()
+start_time = time.time()
+logger.write('Annotation begin')
+logger.write('Anotate on chr{}'.format(args.ch))
+
 anno_file = Anno_file(args.ref_db, args.ch, args.anno_type)
 res_file = Res_file(args.res)
 out_file = Out_file(args.output_dir, args.anno_type, args.ch, args.prefix)
@@ -22,3 +35,5 @@ df = dd.merge(anno_file.dd_df(), top_aanno_file.dd_df(), how='inner', on=['chr',
 
 df.repartition(npartitions=1).to_csv(out_file.out_path(), sep='\t', index=False)
 
+logger.write('  Time elapsed: {:.2f} min'.format((time.time()-start_time)/60))
+logger.write('Annotation done.')
